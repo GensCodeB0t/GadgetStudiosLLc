be525e2dfdbc07340befe89dab3a47764ddc81d2
GensCodeB0t <gensy83@gmail.com>
2018-09-29 00:15:35 -0500

    Update settings.py

--- GadgetStudios/settings.py
+++ GadgetStudios/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'eg5r$a1fcjapakff30xtapvpn)_e#!w)gyi1sbaprkm22t7khd'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
